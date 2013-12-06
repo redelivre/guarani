38fc2bc92177ac11bc6b6f8ac0538262c1017523
Flávio Zavan <flaviozavan@gmail.com>
2013-12-06 12:26:47 -0200

    Corrige menu responsivo

--- js/responsive.js
+++ js/responsive.js
@@ -5,31 +5,36 @@ jQuery(function($){
 		}
 	});
 
-	var liParent = $('.menu li > ul').parent('li');
+	function makeMenuResponsive(liParent)
+	{
+		liParent.each(function(){
+			$(this).children('a').on('click', function(){
+				if($(window).width() < 600) {
+					var liLinkOriginalHTML = '<a href="'+$(this).attr('href')+'">'+$(this).text().replace($(this).text().substr(-1), '')+'</a>';
+					var ulChildren = $(this).next('ul');
+					var content = $(this).parent('li').children('a').next().html();
 
-	liParent.children('a').append(' &raquo;')
+					$('.menu').hide();
+					$('.menu-tmp').hide();
+					$('.menu-main-container').append('<ul class="menu-tmp"><li><a href="#" class="menu-back-live">&laquo; Voltar</a>'+liLinkOriginalHTML+'</li>'+content+'</ul>');
+					makeMenuResponsive($('.menu-tmp li > ul').parent('li'));
 
-	liParent.each(function(){
-		$(this).children('a').on('click', function(){
-			if($(window).width() < 600) {
-				var liLinkOriginalHTML = '<a href="'+$(this).attr('href')+'">'+$(this).text().replace($(this).text().substr(-1), '')+'</a>';
-				var ulChildren = $(this).next('ul');
-				var content = $(this).parent('li').children('a').next().html();
-
-				$('.menu').fadeOut('fast');
-				$('.menu-main-container').append('<ul class="menu-tmp"><li><a href="#" class="menu-back-live">&laquo; Voltar</a>'+liLinkOriginalHTML+'</li>'+content+'</ul>');
-
-				return false;
-			}
+					return false;
+				}
+			});
 		});
-	});
+	}
+
+	var liParent = $('.menu li > ul').parent('li');
+	liParent.children('a').append(' &raquo;');
+	makeMenuResponsive(liParent);
 
 	$('.menu-back-live').live('click', function(){
 		if($(window).width() < 600) {
-			$('.menu-tmp').fadeOut('fast');
-			$('.menu-tmp').remove();
-
-			$('.menu').fadeIn();
+			var menu = $(this).parents('.menu-tmp');
+			var oldMenu = menu.prev();
+			menu.remove();
+			oldMenu.show();
 
 			return false;
 		}
